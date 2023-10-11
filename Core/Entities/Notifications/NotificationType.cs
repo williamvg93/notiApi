ec3538e47aa32438d7ec19784e70ce67b8c13c7d
williamvg93 <sonico_5071@hotmail.com>
2023-10-11 13:15:33 -0500

    feat: :sparkles: Add All Entities, Added notifications Entities

---
+++ Core/Entities/Notifications/NotificationType.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core.Entities.Notifications;
+
+public class NotificationType : BaseEntity
+{
+    public string Name { get; set; }
+    public DateTime CreationDate { get; set; }
+    public DateTime ModificationDate { get; set; }
+}
