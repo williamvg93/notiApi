c7b6a5b8de394ecd2d4b436500296bdace4a188f
williamvg93 <88460050+williamvg93@users.noreply.github.com>
2023-10-13 12:30:22 +0000

    feat: :sparkles: Interfaces, Addded all Interfaces fot Entities(Block, Noti, Person)

---
+++ Core/Interfaces/Notifications/IRequiType.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities.Notifications;
+
+namespace Core.Interfaces.Notifications;
+
+public interface IRequiType : IGenericRepository<RequirementType>
+{
+
+}
---
+++ Core/Interfaces/Person/IGeneSubModu.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities.Person;
+
+namespace Core.Interfaces.Person;
+
+public interface IGeneSubModu : IGenericRepository<GenericSubModules>
+{
+
+}
